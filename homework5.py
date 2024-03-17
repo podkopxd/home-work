2fea9cf21d097fd1c9131cb05796f4c7a84e17dd
podkopxd <163719825+podkopxd@users.noreply.github.com>
2024-03-17 18:18:36 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+MyList = ['dog', 'cat', 'fish', 'elephant', 'spider', 'bear']
+print(MyList)
+print(MyList[0:6:5])
+MyList[2] = 'squid'
+print(MyList)
+
+MyDict = {'Nika': 35467898675645, 'Lesha': 9876554677865, 'Roma': 56789876545432, 'Loshok': 88005553535, 'Artem': 987654467865645}
+print(MyDict)
+print(MyDict['Loshok'])
+MyDict['Sasha'] = 654567899876
+print(MyDict)
