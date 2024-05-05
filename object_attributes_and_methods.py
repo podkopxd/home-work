2daa2ea7d1b97246dcf4eeb7b799259670bfab47
podkopxd <163719825+podkopxd@users.noreply.github.com>
2024-05-05 17:51:52 +0300

    Update object_attributes_and_methods.py

--- object_attributes_and_methods.py
+++ object_attributes_and_methods.py
@@ -1,7 +1,10 @@
 class House:
-    def __init__(self):
-        self.numberOfFloors = 10
+    numberOfFloors = 10
+
+    def print_floors(self):
+        for floor in range(1, self.numberOfFloors + 1):
+            print("Текущий этаж равен", floor)
 
 
 my_house = House()
-print('Current floor is: ', my_house.numberOfFloors)
+my_house.print_floors()
