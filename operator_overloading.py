13e2924a9ab7f44d9cbc80e89589c88ab482ddf5
podkopxd <163719825+podkopxd@users.noreply.github.com>
2024-05-05 17:55:14 +0300

    Update operator_overloading.py

--- operator_overloading.py
+++ operator_overloading.py
@@ -1,11 +1,9 @@
 class Building:
-    def __init__(self, floors, type):
-        self.numberOfFloors = floors
-        self.buildingType = type
+    def __init__(self, numberOfFloors, buildingType):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
     def __eq__(self, other):
-        return self is other
-
-my_house = Building(9, None)
-is_eq = my_house.buildingType == my_house.numberOfFloors
-print(is_eq)
+        if isinstance(other, Building):
+            return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
+        return False
