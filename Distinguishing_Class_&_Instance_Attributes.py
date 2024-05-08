cd10fbfdb8887a0378da5aa1b26d44b1583094b9
podkopxd <163719825+podkopxd@users.noreply.github.com>
2024-05-08 13:26:08 +0300

    Update Distinguishing_Class_&_Instance_Attributes.py

--- Distinguishing_Class_&_Instance_Attributes.py
+++ Distinguishing_Class_&_Instance_Attributes.py
@@ -4,11 +4,6 @@ class Building:
     def __init__(self):
         Building.total += 1
 
-all_buildings = []
-
-while len(all_buildings) < 40:
+for i in range(40):
     new_building = Building()
-    all_buildings.append(new_building)
-
-print(all_buildings)
-print(Building.total)
+print("Total number of buildings", Building.total)
